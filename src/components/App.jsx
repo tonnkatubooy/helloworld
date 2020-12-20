573fcc9f2e26b631a785f2d197d7afa35a7f3f86
tonnkatubooy <tonnkatubooy@yahoo.co.jp>
2020-12-20 13:52:13 +0900

    addList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Form from './Form';
+import List from './List';
 
 export default class App extends Component {
   render() {
@@ -7,6 +8,8 @@ export default class App extends Component {
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
         <Form/>
+        <div className="siimple-rule"></div>
+        <List/>
       </div>
     );
   }
